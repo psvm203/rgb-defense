0424cbeb72bedd3566fe4e2db599892e2603ff68
psvm203 <psvm203@proton.me>
2026-07-05 18:38:21 +0900

    feat(ui): add master volume slider to settings

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -4,6 +4,7 @@ extends CanvasLayer
 @onready var _settings_panel: VBoxContainer = $SettingsPanel
 @onready var _res_option: OptionButton = $SettingsPanel/ResHBox/ResOption
 @onready var _fullscreen_check: CheckButton = $SettingsPanel/FullscreenCheck
+@onready var _volume_slider: HSlider = $SettingsPanel/VolumeHBox/VolumeSlider
 
 const RESOLUTIONS: Array[Vector2i] = [
 	Vector2i(960, 540),
@@ -13,6 +14,9 @@ const RESOLUTIONS: Array[Vector2i] = [
 const CONFIG_SECTION := "display"
 const CONFIG_RES_INDEX := "resolution_index"
 const CONFIG_FULLSCREEN := "fullscreen"
+const CONFIG_VOLUME := "volume"
+const MASTER_BUS := 0
+const MIN_VOLUME_DB := -40.0
 
 
 func _ready() -> void:
@@ -48,6 +52,11 @@ func _on_fullscreen_toggled(_on: bool) -> void:
 	_apply_settings()
 
 
+func _on_volume_changed(_value: float) -> void:
+	_apply_volume()
+	_save_settings()
+
+
 func _apply_settings() -> void:
 	var index := _res_option.selected
 	var size := RESOLUTIONS[index]
@@ -56,12 +65,19 @@ func _apply_settings() -> void:
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		DisplayServer.window_set_size(size)
+	_apply_volume()
+
+
+func _apply_volume() -> void:
+	var db := lerpf(MIN_VOLUME_DB, 0.0, _volume_slider.value / 100.0)
+	AudioServer.set_bus_volume_db(MASTER_BUS, db)
 
 
 func _save_settings() -> void:
 	var config := ConfigFile.new()
 	config.set_value(CONFIG_SECTION, CONFIG_RES_INDEX, _res_option.selected)
 	config.set_value(CONFIG_SECTION, CONFIG_FULLSCREEN, _fullscreen_check.button_pressed)
+	config.set_value(CONFIG_SECTION, CONFIG_VOLUME, _volume_slider.value)
 	config.save("user://settings.cfg")
 
 
@@ -71,3 +87,4 @@ func _load_settings() -> void:
 		return
 	_res_option.selected = config.get_value(CONFIG_SECTION, CONFIG_RES_INDEX, 0)
 	_fullscreen_check.button_pressed = config.get_value(CONFIG_SECTION, CONFIG_FULLSCREEN, false)
+	_volume_slider.value = config.get_value(CONFIG_SECTION, CONFIG_VOLUME, 100.0)
