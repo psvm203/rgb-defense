65527e0aaf257a2ff4130716df69c10d4a6b66f6
psvm203 <psvm203@proton.me>
2026-07-05 18:36:28 +0900

    feat(ui): add settings panel with resolution and fullscreen options

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -1,12 +1,73 @@
 extends CanvasLayer
 
+@onready var _menu_panel: VBoxContainer = $MenuPanel
+@onready var _settings_panel: VBoxContainer = $SettingsPanel
+@onready var _res_option: OptionButton = $SettingsPanel/ResHBox/ResOption
+@onready var _fullscreen_check: CheckButton = $SettingsPanel/FullscreenCheck
+
+const RESOLUTIONS: Array[Vector2i] = [
+	Vector2i(960, 540),
+	Vector2i(1280, 720),
+	Vector2i(1920, 1080),
+]
+const CONFIG_SECTION := "display"
+const CONFIG_RES_INDEX := "resolution_index"
+const CONFIG_FULLSCREEN := "fullscreen"
+
+
+func _ready() -> void:
+	_load_settings()
+	_apply_settings()
+
+
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://track/track.tscn")
 
 
 func _on_settings_pressed() -> void:
-	pass
+	_menu_panel.visible = false
+	_settings_panel.visible = true
+
+
+func _on_back_pressed() -> void:
+	_settings_panel.visible = false
+	_menu_panel.visible = true
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_resolution_changed(_index: int) -> void:
+	_save_settings()
+	_apply_settings()
+
+
+func _on_fullscreen_toggled(_on: bool) -> void:
+	_save_settings()
+	_apply_settings()
+
+
+func _apply_settings() -> void:
+	var index := _res_option.selected
+	var size := RESOLUTIONS[index]
+	if _fullscreen_check.button_pressed:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		DisplayServer.window_set_size(size)
+
+
+func _save_settings() -> void:
+	var config := ConfigFile.new()
+	config.set_value(CONFIG_SECTION, CONFIG_RES_INDEX, _res_option.selected)
+	config.set_value(CONFIG_SECTION, CONFIG_FULLSCREEN, _fullscreen_check.button_pressed)
+	config.save("user://settings.cfg")
+
+
+func _load_settings() -> void:
+	var config := ConfigFile.new()
+	if config.load("user://settings.cfg") != OK:
+		return
+	_res_option.selected = config.get_value(CONFIG_SECTION, CONFIG_RES_INDEX, 0)
+	_fullscreen_check.button_pressed = config.get_value(CONFIG_SECTION, CONFIG_FULLSCREEN, false)
